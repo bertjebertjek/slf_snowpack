d408c3917f84fcdd37ed3877bc3c52bd4ce68b03
Mathias Bavay <bavay@slf.ch>
2024-04-17 07:24:27 +0200

    Merging some changes from MeteoIO's version of this file

--- tools/cmake/SetCompilerOptions.cmake
+++ tools/cmake/SetCompilerOptions.cmake
@@ -1,3 +1,4 @@
+#SPDX-License-Identifier: LGPL-3.0-or-later
 #Set different variables according to the detected compiler and processor
 #based on $CMAKE_CXX_COMPILER_ID it sets the following variables:
 # WARNINGS, EXTRA_WARNINGS, EXTRA, OPTIM, ARCH, DEBUG, _VERSION, PROFILING
@@ -6,17 +7,25 @@
 INCLUDE("${CMAKE_SOURCE_DIR}/tools/cmake/BuildVersion.cmake")
 BuildVersionGIT()
 
+#TODO: replace all defs such as /D__DEBUG or -DDEBUG_ARITHM by add_compile_definitions() once moving to cmake >=3.12 (released 11.2018)
+
 MACRO (SET_COMPILER_OPTIONS)
+	SET(USER_COMPILER_OPTIONS "" CACHE STRING "Provide some extra compiler options")
+	MARK_AS_ADVANCED(FORCE USER_COMPILER_OPTIONS)
+	SET(EXTRA "${EXTRA} ${USER_COMPILER_OPTIONS}")
+
 	###########################################################
 	IF(CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
 		SET(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS ON)	#this is required for building libraries
+		SET(EXTRA "${EXTRA} /D_USE_MATH_DEFINES")	#USE_MATH_DEFINES needed for VC++
 		IF(DEBUG_ARITHM)
 			SET(EXTRA "${EXTRA} /EHa")
 		ENDIF(DEBUG_ARITHM)
 		
-		#SET(CMAKE_CONFIGURATION_TYPES "Debug;Release" CACHE STRING "limited configs"  FORCE)
+		#SET(CMAKE_CONFIGURATION_TYPES "Debug;Release" CACHE STRING "limited configs" FORCE)
 		SET(WARNINGS "/W4 /D_CRT_SECURE_NO_WARNINGS /EHsc") #Za: strict ansi EHsc: handle c++ exceptions /w35045: inform about Spectre mitigation
 		#SET(EXTRA_WARNINGS "/Wp64") #/Wall
+		SET(WARNINGS "${WARNINGS} /experimental:external /external:I c:/Windows /external:W0")
 		SET(OPTIM "/O2 /DNDEBUG /DEBUG:FASTLINK /MD /DNOSAFECHECKS")
 		SET(ARCH_OPTIM "/arch:AVX2")
 		SET(ARCH_SAFE "")
@@ -41,7 +50,7 @@ MACRO (SET_COMPILER_OPTIONS)
 		SET(EXTRA_WARNINGS "-Wextra -pedantic ${DEEP_WARNINGS}")
 		SET(OPTIM "-g -O3 -DNDEBUG -DNOSAFECHECKS")
 		IF(CMAKE_SYSTEM_PROCESSOR MATCHES "x86_64" OR CMAKE_SYSTEM_PROCESSOR MATCHES "AMD64")
-			SET(ARCH_SAFE  "-march=nocona -mtune=nocona")
+			SET(ARCH_SAFE "-march=nocona -mtune=nocona")
 			SET(ARCH_OPTIM "-march=native -mtune=native")
 		ENDIF()
 		SET(DEBUG "-g3 -O0 -D__DEBUG")
@@ -61,7 +70,7 @@ MACRO (SET_COMPILER_OPTIONS)
 		SET(OPTIM "-O3 -hfp3 -h msglevel_4 -DNDEBUG -DNOSAFECHECKS")
 		IF($ENV{CRAY_CPU_TARGET} MATCHES "^$")
 			IF(CMAKE_SYSTEM_PROCESSOR MATCHES "x86_64" OR CMAKE_SYSTEM_PROCESSOR MATCHES "AMD64")
-				SET(ARCH_SAFE  "-h cpu=x86-64")
+				SET(ARCH_SAFE "-h cpu=x86-64")
 				MESSAGE("No CRAY_CPU_TARGET set, setting it to x86-64; please consider loading the proper target module.")
 			ELSE()
 				MESSAGE("No CRAY_CPU_TARGET set; please consider loading the proper target module.")
@@ -83,12 +92,12 @@ MACRO (SET_COMPILER_OPTIONS)
 			SET(EXTRA "${EXTRA} -DDEBUG_ARITHM")
 		ENDIF(DEBUG_ARITHM)
 		
-		SET(WARNINGS "-Wall -Wno-long-long  -Wswitch -Wno-unknown-pragmas")
+		SET(WARNINGS "-Wall -Wno-long-long -Wswitch") #-Wno-unknown-pragmas
 		SET(DEEP_WARNINGS "-Wunused-value -Wshadow -Wpointer-arith -Wconversion -Winline -Wdisabled-optimization -Wctor-dtor-privacy") #-Wfloat-equal -Wpadded
 		SET(EXTRA_WARNINGS "-Wextra -pedantic -Weffc++ ${DEEP_WARNINGS}")
 		SET(OPTIM "-g -O3 -DNDEBUG -DNOSAFECHECKS")
 		IF(CMAKE_SYSTEM_PROCESSOR MATCHES "x86_64" OR CMAKE_SYSTEM_PROCESSOR MATCHES "AMD64")
-			SET(ARCH_SAFE  "-march=nocona -mtune=nocona")
+			SET(ARCH_SAFE "-march=x86-64-v2 -mtune=core-avx2")
 		ENDIF()
 		SET(DEBUG "-g3 -O0 -D__DEBUG")
 		SET(_VERSION "-D_VERSION=${_versionString}")
@@ -104,7 +113,7 @@ MACRO (SET_COMPILER_OPTIONS)
 		ENDIF(NOT ANDROID)
 		EXECUTE_PROCESS(COMMAND ${CMAKE_CXX_COMPILER} -dumpversion OUTPUT_VARIABLE GCC_VERSION)
 		IF(GCC_VERSION VERSION_GREATER 4.2 OR GCC_VERSION VERSION_EQUAL 4.2)
-			SET(ARCH_OPTIM  "-march=native -mtune=native")
+			SET(ARCH_OPTIM "-march=native -mtune=native")
 		ENDIF()
 		IF(GCC_VERSION VERSION_GREATER 4.8 OR GCC_VERSION VERSION_EQUAL 4.8)
 			SET(EXTRA_WARNINGS "${EXTRA_WARNINGS} -Wvector-operation-performance") #for gcc>=4.7.0
@@ -114,9 +123,12 @@ MACRO (SET_COMPILER_OPTIONS)
 				FIND_PROGRAM(CMAKE_GCC_NM NAMES ${_CMAKE_TOOLCHAIN_PREFIX}gcc-nm HINTS ${_CMAKE_TOOLCHAIN_LOCATION})
 				FIND_PROGRAM(CMAKE_GCC_RANLIB NAMES ${_CMAKE_TOOLCHAIN_PREFIX}gcc-ranlib HINTS ${_CMAKE_TOOLCHAIN_LOCATION})
 
-				IF( CMAKE_GCC_AR AND CMAKE_GCC_NM AND CMAKE_GCC_RANLIB )
-					SET(OPTIM "${OPTIM} -flto=auto") 
-					SET( CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -flto -fno-fat-lto-objects" )
+				IF(CMAKE_GCC_AR AND CMAKE_GCC_NM AND CMAKE_GCC_RANLIB)
+					SET(USE_LTO_OPTIMIZATIONS ON CACHE BOOL "Use Link Time Optmizations when compiling (memory heavy while compiling)")
+					MARK_AS_ADVANCED(FORCE USE_LTO_OPTIMIZATIONS)
+					IF(USE_LTO_OPTIMIZATIONS)
+						SET(OPTIM "${OPTIM} -flto=auto -fno-fat-lto-objects")
+					ENDIF()
 					SET( CMAKE_AR "${CMAKE_GCC_AR}" )
 					SET( CMAKE_NM "${CMAKE_GCC_NM}" )
 					SET( CMAKE_RANLIB "${CMAKE_GCC_RANLIB}" )
@@ -155,7 +167,7 @@ MACRO (SET_COMPILER_OPTIONS)
 			SET(EXTRA "${EXTRA} -DDEBUG_ARITHM")
 		ENDIF(DEBUG_ARITHM)
 		
-		SET(WARNINGS_OFF "-Wno-long-long -Wno-float-equal -Wno-documentation -Wno-documentation-unknown-command -Wno-old-style-cast -Wno-padded -Wno-missing-noreturn -Wno-weak-vtables -Wno-switch-enum -Wno-covered-switch-default -Wno-global-constructors -Wno-exit-time-destructors -Wno-unknown-pragmas -Wno-format-nonliteral -Wno-date-time -Wno-unused-template")
+		SET(WARNINGS_OFF "-Wno-long-long -Wno-float-equal -Wno-documentation -Wno-documentation-unknown-command -Wno-old-style-cast -Wno-padded -Wno-missing-noreturn -Wno-weak-vtables -Wno-switch-enum -Wno-covered-switch-default -Wno-global-constructors -Wno-exit-time-destructors -Wno-unknown-pragmas -Wno-format-nonliteral -Wno-date-time -Wno-unused-template -Wno-disabled-macro-expansion -Wno-c++98-compat -Wno-c++98-compat-pedantic")
 		SET(WARNINGS "-Wall -Wswitch -Weverything ${WARNINGS_OFF}") #obviously, we should try to fix the warnings! Keeping in mind that some of these W are half buggy...
 		SET(DEEP_WARNINGS "-Wunused-value -Wshadow -Wpointer-arith -Wconversion -Winline -Wdisabled-optimization -Wctor-dtor-privacy") #-Rpass=.* for static analysis
 		SET(EXTRA_WARNINGS "-Wextra -pedantic -Weffc++ ${DEEP_WARNINGS}")
@@ -167,7 +179,7 @@ MACRO (SET_COMPILER_OPTIONS)
 			ENDIF()
 		ELSE(APPLE)
 			IF(CMAKE_SYSTEM_PROCESSOR MATCHES "x86_64" OR CMAKE_SYSTEM_PROCESSOR MATCHES "AMD64")
-				SET(ARCH_SAFE "-march=nocona -mtune=nocona")
+				SET(ARCH_SAFE "-march=x86-64-v2 -mtune=core-avx2")
 			ENDIF()
 		ENDIF(APPLE)
 		SET(DEBUG "-g3 -O0 -D__DEBUG")
@@ -179,7 +191,7 @@ MACRO (SET_COMPILER_OPTIONS)
 			IF (LEAKS_CHECK)
 				SET(EXTRA "${EXTRA} -ftrapv -fno-omit-frame-pointer") #-fsanitize=address,undefined,integer,undefined-trap but this is currently not supported by Apple
 			ENDIF(LEAKS_CHECK)
-		SET(ARCH_OPTIM  "-march=native")
+		SET(ARCH_OPTIM "-march=native")
 	ENDIF()
 	
 	###########################################################
@@ -189,11 +201,11 @@ MACRO (SET_COMPILER_OPTIONS)
 	ENDIF(NOT DEST)
 
 	IF (DEST STREQUAL "safe")
-		SET(ARCH  "${ARCH_SAFE}")
+		SET(ARCH "${ARCH_SAFE}")
 	ENDIF(DEST STREQUAL "safe")
 
 	IF(DEST STREQUAL "optimized")
-		SET(ARCH  "${ARCH_OPTIM}")
+		SET(ARCH "${ARCH_OPTIM}")
 	ENDIF(DEST STREQUAL "optimized")
 
 	#show exception messages in a graphical message box
