bb121ba89ca40750ade3bc9a1812f44a48287fb7
Nander Wever <nander.wever@slf.ch>
2024-05-15 15:57:57 +0200

    Fixing formatting and missing throw I assume.

--- snowpack/Hazard.cc
+++ snowpack/Hazard.cc
@@ -117,8 +117,8 @@ void Hazard::actOnVector(std::vector<double>& oldVector, const double& newValue,
 			break;
 		case noAction:
 			break;
-    default:
-      InvalidArgumentException("Unknown action provided to actOnVector", AT);
+		default:
+			throw InvalidArgumentException("Unknown action provided to actOnVector", AT);
 	}
 }
 
