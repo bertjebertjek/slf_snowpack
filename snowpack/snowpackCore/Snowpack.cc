beb4a0b6887cbe2ffabd00ee9e17e856f4534b79
Nander Wever <nander.wever@slf.ch>
2025-05-19 17:04:13 +0200

    Improving comment with details from the previous commit message.

--- snowpack/snowpackCore/Snowpack.cc
+++ snowpack/snowpackCore/Snowpack.cc
@@ -1327,7 +1327,8 @@ bool Snowpack::compTemperatureProfile(const CurrentMeteo& Mdata, SnowStation& Xd
 		// Ensure that when top element consists of ice, its upper node does not exceed melting temperature
 		// This is to have consistent surface energy balance calculation and for having good looking output
 		if (variant == "SEAICE") {
-			// For sea ice, check if the phase change in the surface element is significant, such that tiny amounts of brine salinity doesn't cause problems. Note that (1.E-5) comes from the convergence criterion
+			// For sea ice, check if the phase change in the surface element is significant, such that tiny amounts of brine salinity doesn't cause problems. Note that (1.E-5) comes from the convergence criterion.
+			// The issue can be that tiny amounts of brine salinity can create very low melting points, also because the phase change associated with the brine salinity is too small for the convergence criterion, and the thermal equilibrium is also never balanced (i.e., thermal equilibrium is not reached, because involved phase changes are too small).
 			if (nE > 0 && Xdata.Edata[nE-1].theta[ICE] > Constants::eps && Xdata.Edata[nE-1].Qmf / ((Constants::density_ice * Constants::lh_fusion) / sn_dt) > (1.E-5)) NDS[nE].T=std::min(Xdata.Edata[nE-1].meltfreeze_tk, NDS[nE].T);
 			// Just make sure we never exceed the fresh-water melting point for sea ice surface temperatures
 			NDS[nE].T=std::min(Constants::meltfreeze_tk, NDS[nE].T);
