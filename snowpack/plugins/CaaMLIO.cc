d98ecd3c3d8f87f9f6ebe3c85dc1c1a670959b73
Mathias Bavay <bavay@slf.ch>
2025-06-10 11:47:15 +0200

    Relying on RunInfo to provide the history metadata so it is more consistent accross output formats.
    Moreover, since the RunInfo object is initialized once and forwarded to
    all plugins, it should now be possible to use the run date even in
    Alpine3D without performance penalty.

--- snowpack/plugins/CaaMLIO.cc
+++ snowpack/plugins/CaaMLIO.cc
@@ -1053,7 +1053,7 @@ void CaaMLIO::writeSnowCover(const Date& date, const SnowStation& Xdata,
 	std::string hazfilename( getFilenamePrefix(Xdata.meta.getStationID().c_str(), o_snowpath) + bak + ".haz" );
 
 	writeSnowFile(snofilename, date, Xdata);
-	if (haz_write) SmetIO::writeHazFile(hazfilename, date, Xdata, Zdata);
+	if (haz_write) SmetIO::writeHazFile(hazfilename, date, Xdata, Zdata, info.history);
 }
 
 /**
@@ -1094,8 +1094,7 @@ void CaaMLIO::writeSnowFile(const std::string& snofilename, const Date& date, co
 	pugi::xml_node srcNode = root.append_child( (namespaceCAAML+":srcRef").c_str() )
 	                             .append_child( (namespaceCAAML+":Operation").c_str() );
 	srcNode.append_attribute("gml:id") = "OPERATION_ID";
-	const std::string snowpackString = "SNOWPACK v"+info.version+" compiled on "+info.compilation_date+". user: "+info.user;
-	xmlWriteElement(srcNode,(namespaceCAAML+":name").c_str(),snowpackString.c_str(),"","");
+	xmlWriteElement(srcNode, (namespaceCAAML+":name").c_str(), info.history.c_str(), "", "");
 
 	// Write station data locRef
 	writeStationData(root,Xdata);
